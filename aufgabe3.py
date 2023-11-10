8e98372f94e3c96a297faecb508a4555af81cc92
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2023-11-10 10:59:45 +0100

    Teil2 Aufgabe 3 Math Modul

---
+++ aufgabe3.py
@@ -0,0 +1,8 @@
+import math
+print("Potenzen von 1 bis 10:")
+for i in range(1,11):
+    print(math.pow(i,2))
+    
+print("\nWurzel von 10 bis 100 in 10er Schritten:")
+for i in range(1,11):
+    print(math.sqrt(i*10))

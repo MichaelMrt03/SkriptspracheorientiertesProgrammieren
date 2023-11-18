2c0c941eb26a9fc8e1f19e0a9a2eb0607d0e15ab
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2023-11-18 15:04:00 +0100

    aufgabe 1.1

---
+++ Teil3/aufgabe1.py
@@ -0,0 +1,10 @@
+import re
+
+#1. Das Wort "Python" oder "python" am Anfang einer Zeile
+daten = "Python am Anfang"
+muster = "^[Pp]ython"
+ergebnis = re.search(muster,daten)
+if ergebnis:
+    print("gefunden")
+else:
+    print("nicht gefunden")

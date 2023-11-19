4abf05033680baa50447932ef1b2dbb8b4a3e01c
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2023-11-19 15:44:58 +0100

    Teil 3 Aufgabe 1.2

--- Teil3/aufgabe1.py
+++ Teil3/aufgabe1.py
@@ -1,8 +1,18 @@
 import re
 
 #1. Das Wort "Python" oder "python" am Anfang einer Zeile
+"""
 daten = "Python am Anfang"
 muster = "^[Pp]ython"
+ergebnis = re.search(muster,daten)
+if ergebnis:
+    print("gefunden")
+else:
+    print("nicht gefunden")
+"""
+#2 Den Inhalt einer gesamten Python - Kommentarzeile
+daten = "#Das ist mein Beispielkommentar in Python"
+muster = r"^#\s*[^#\n]*$"
 ergebnis = re.search(muster,daten)
 if ergebnis:
     print("gefunden")

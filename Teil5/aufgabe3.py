5f1a19d72ad351aba169f5b90f63d5884d8cf974
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2023-12-21 16:43:29 +0100

    Histogramme erzeugen, Graustufen und Farbstufen aus Bildern anzeigen lassen

--- Teil5/aufgabe3.py
+++ Teil5/aufgabe3.py
@@ -1,19 +1,31 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
+def graustufenhistogramm():
+    plt.hist(img.ravel(),256,[0,256])
+    plt.show()
+
+def farbhistogramm():
+     color = ('b','g','r')
+     for i,col in enumerate(color):
+         histr = cv2.calcHist([img],[i],None,[256],[0,256])
+         plt.plot(histr,color=col)
+         plt.xlim([0,256])
+         plt.show()
 
 def loop():
     k = cv2.waitKey(0)
     if k==27:
         cv2.destroyAllWindows()
     elif k==49:#1
-        #hist
+        graustufenhistogramm()
     elif k==50:#2
-        #hist
+        farbhistogramm()
     else:
         cv2.imshow('myimg',img)
         loop()
         
-img = cv2.imread("Bilder/ekg.jpg")
+img = cv2.imread("Bilder/geschwindigkeit2.jpg")
 cv2.imshow('myimg',img)
 loop()

e2fd6cf7b4c75190fd500a31d606f14066ca7f17
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-01-13 21:01:49 +0100

    Kleine Anpassung der Aufgabe

--- Teil3/aufgabe1.3.py
+++ Teil3/aufgabe1.3.py
@@ -1,10 +1,11 @@
 import re
 #Ein Zeichen, das eine Ziffer oder ein Vorzeichen (also + oder ‐) ist
-daten = "9"
+daten = "+"
 muster = r"^[0-9,+-]$"
 
 ergebnis = re.search(muster,daten)
 if ergebnis:
     print("gefunden")
+    print(ergebnis.group())
 else:
     print("nicht gefunden")

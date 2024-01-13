ed32e62d61d5fd4f5bf58f8249bc9a8d6e2d3115
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2024-01-13 21:01:22 +0100

    Aufgabe überarbeitet

--- Teil3/aufgabe1.2.py
+++ Teil3/aufgabe1.2.py
@@ -1,7 +1,7 @@
 import re
 #2 Den Inhalt einer gesamten Python - Kommentarzeile
-daten = "#Das ist mein Beispielkommentar in Python"
-muster = r"^#\s*[^#\n]*$"
+daten = "#python okok jajaj 1232131231"
+muster = r"^.*"
 ergebnis = re.search(muster,daten)
 if ergebnis:
     print("gefunden")
